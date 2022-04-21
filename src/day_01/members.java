66c87b05366a011119a72f5347d92881aefc1471
Ahmet Yoruk <ahmet@soilconnect.com>
2022-04-21 14:58:31 -0400

    added to members

--- src/day_01/members.java
+++ src/day_01/members.java
@@ -3,5 +3,10 @@ package day_01;
 public class members {
     public static void main(String[] args) {
         System.out.println("HelloBaam");
+        //ahmet yoruk
+        //alev
+        //busra
+        //mert
+
     }
 }
