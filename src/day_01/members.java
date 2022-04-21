c0ce5af74740941be5f91f92887c9d31086e05a3
metin <metinmertkirdim@hotmail.com>
2022-04-21 14:59:03 -0400

    Mert updated members file

--- src/day_01/members.java
+++ src/day_01/members.java
@@ -4,7 +4,7 @@ public class members {
     public static void main(String[] args) {
         System.out.println("HelloBaam");
 
-        System.out.println("Highest stakeholder is Mert with %40 :D");
+        System.out.println("Highest stakeholder is Mert with %40");
 
 
 
