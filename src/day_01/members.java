dd9c7abbb5896e0b55989ce9dee67dbf0dab29cc
Alevn <alevny@hotmail.com>
2022-04-21 14:58:28 -0400

    updated

--- src/day_01/members.java
+++ src/day_01/members.java
@@ -3,5 +3,15 @@ package day_01;
 public class members {
     public static void main(String[] args) {
         System.out.println("HelloBaam");
+        System.out.println("Alev");
+        System.out.println("Cinozgumus");
+
+
+
+
+
+
+
+
     }
 }
